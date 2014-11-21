4fb70e13898c3f790970b8684e49cea4b469f917
alexandre-pereira <alexandre.pereira.it@gmail.com>
2014-11-21 15:32:37 +0100

    v0.01

--- Developpement/DreamPark/Model/Parking/Place.py
+++ Developpement/DreamPark/Model/Parking/Place.py
@@ -1,4 +1,8 @@
 class Place:
 
-    def __init__(self, id):
+    def __init__(self, id, hauteur, largeur, longueur, voiture=None):
         self.__id = id
+        self.__hauteur = hauteur
+        self.__largeur = largeur
+        self.__longueur = longueur
+        self.__voiture = voiture
--- Developpement/DreamPark/app.py
+++ Developpement/DreamPark/app.py
@@ -1,2 +1,15 @@
+from Model.Parking.Parking import Parking
+from Model.Parking.Place import Place
 
-print("coucou Alex! :)")
+print("Démarrage...")
+
+places = []
+
+for i in range(0,10) :
+    places.append(Place(id, 1, 1, 1, voiture=None))
+
+
+p1 = Parking(places)
+
+for p in p1.places:
+    print(p)
