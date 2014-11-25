6eca7c749e6c829581920683f273f15375596be2
alexandre-pereira <alexandre.pereira.it@gmail.com>
2014-11-25 16:07:48 +0100

    ok
    ok

---
+++ Developpement/DreamPark/Model/Parking/Placement.py
@@ -0,0 +1,8 @@
+class Placement:
+
+    def __init__(self, place, voiture, client, dateD, dateF=None):
+        self.__voiture = voiture
+        self.__place = place
+        self.__client = client
+        self.__dateD = dateD
+        self.__dateF = dateF
--- Developpement/DreamPark/Model/Voiture.py
+++ Developpement/DreamPark/Model/Voiture.py
@@ -1,7 +1,10 @@
 class Voiture:
 
-    def __init__(self, longueur, largeur, hauteur):
+    def __init__(self, longueur, largeur, hauteur, immatriculation):
         self.__longueur = longueur
         self.__largeur = largeur
         self.__hauteur = hauteur
+        self.__immatriculation = immatriculation
 
+    def __str__(self):
+         return "foo"
