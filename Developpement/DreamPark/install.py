641ff0b59410570bb0d3f87d77f6fdc0d023240d
alexandre-pereira <alexandre.pereira.it@gmail.com>
2014-12-05 14:30:32 +0100

    ss

--- Developpement/DreamPark/install.py
+++ Developpement/DreamPark/install.py
@@ -1,14 +1,29 @@
 from Model.Parking import Parking, Place
 
-print("Démarrage...")
+print("Définition des places de bases...")
 
 places = []
+hauteurs = ['a', 'b', 'c', 'd', 'e']
+print(random.choice(foo))
+for i in range(0,20) :
+    places.append(Place(i, 1, uniform(1.2, 2.2), uniform(1, 2)))
 
-for i in range(0,10) :
-    places.append(Place(i, 1, 1, 1))
+
+(self, id, niveau, hauteur, largeur, longueur
+
+    def capturerHauteur(self):
+        return uniform(1, 2)
+
+    def capturerLongueur(self):
+        return uniform(2, 4)
+
+    def capturerLargeur(self):
+        return uniform(1.3, 2.2)
 
 
 p1 = Parking(places)
 
 for p in p1.places:
     print(p)
+
+print("OK\n")
