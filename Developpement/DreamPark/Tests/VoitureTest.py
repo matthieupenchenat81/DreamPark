8493b6e4eb6a07e2ac115c30ad7f951aa6cf6d8d
Malibu <matthieu-p93@live.fr>
2015-01-31 17:14:30 +0100

    views

--- Developpement/DreamPark/Tests/VoitureTest.py
+++ Developpement/DreamPark/Tests/VoitureTest.py
@@ -1,10 +1,28 @@
 import unittest
-
+from Developpement.DreamPark.Model.Parking.Voiture import Voiture
 
 class VoitureTest(unittest.TestCase):
-    def test_get_element(self):
-        simple_comme_bonjour = ('pomme', 'banane')
-        self.assertEqual('pomme', 'pomme')
+
+    def test_create_voiture(self):
+        v= Voiture()
+        self.assertNotEquals(v.immatriculation, None)
+        self.assertNotEquals(v.hauteur, None)
+        self.assertNotEquals(v.largeur, None)
+        self.assertNotEquals(v.longueur, None)
+
+    def test_get_voiture(self):
+        v= Voiture()
+        self.assertEquals(v, Voiture.get(v.immatriculation))
+
+    def test_get_dim(self):
+        v= Voiture("IMMAT", 100, 200, 300)
+        self.assertEquals(v.hauteur, 300)
+        self.assertEquals(v.longueur, 100)
+        self.assertEquals(v.largeur, 200)
+
+    def test_get_id(self):
+        v= Voiture("IMMAT", 100, 200, 300)
+        self.assertEquals(v.immatriculation, "IMMAT")
 
 
 if __name__ == '__main__':
